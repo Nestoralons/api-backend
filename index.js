93a8300ee2e9862a851a613fabe7cb51152c737b
Nestoralons <nestoralons42@gmail.com>
2022-08-27 03:08:17 -0400

    Add Heroku needed config

--- index.js
+++ index.js
@@ -70,6 +70,6 @@ app.use((request, response) => {
     error: "Not found",
   });
 });
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
